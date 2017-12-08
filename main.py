cebfdc00b7a180a36e0822fe600b6af8fdfeb782
Henrique Cancado <henriquecanp@gmail.com>
2017-12-08 18:42:07 -0200

    Atualizacao do custo_fixo com cenarios 1 2 3 e custo por pedido mais

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
 
     #parâmetros deterministicos
     dados['H'] = 96         #horizonte de tempo (em semanas) 
-    dados['h0'] = 3000      #estoque inicial
-    dados['Cp'] = 50        #custo de compra de uma unidade
-    dados['Cs'] = 500       #custo de pedido (setup)
+    dados['h0'] = 2000      #estoque inicial
+    dados['Cp'] = 800        #custo de compra de uma unidade
+    dados['Cs'] = 5000       #custo de pedido (setup)
     dados['Cf'] = 100000    #custo fixo de faltante
-    dados['Cv'] = 100       #custo variavel de faltante
+    #dados['Cv'] = 100       #custo variavel de faltante
     dados['i'] = 0.2        #taxa de interesse
 
     #parâmetros de variáveis estocásticas
@@ -46,40 +46,43 @@ if __name__ == "__main__":
     ######### Simulação dos resultados ##########
     
     #>>>>> Cenário - 1: Situação normal
-    custos = []
-    for i in range(100):
-        demanda = gera_demanda(1, dados)
-        ct = simula(dados, melhor_E+1000, melhor_T, demanda)
-        custos.append(ct)
+    #custos = []
+    #for i in range(100):
+    #    demanda = gera_demanda(1, dados)
+    #    ct = simula(dados, melhor_E, melhor_T, demanda)
+    #    custos.append(ct)
     
     print("Otimização: {:f}".format(melhor_custo))
-    print("Simulação: {:f}".format(np.mean(custos)))
+    #print("Simulação: {:f}".format(np.mean(custos)))
 
     print(melhor_T, melhor_E)
 
     #>>>>> Cenário - 2: A média da demanda pode aumentar
-    #demanda = gera_damanda(2, dados)
     #custos = []
     #for i in range(100):
-    #    ct = simula(dados, E, T, demanda)
+    #    demanda = gera_demanda(2, dados)
+    #    ct = simula(dados, melhor_E, melhor_T, demanda)
     #    custos.append(ct)
     #print(np.mean(custos))
    
     #>>>>> Cenário - 3: A média da demenda pode diminuir
-    #demanda = gera_damanda(3, dados)
-    #custos = []
-    #for i in range(100):
-    #    ct = simula(dados, E, T, demanda)
-    #    custos.append(ct)
-    #print(np.mean(custos))
-  
+    custos = []
+    for i in range(100):
+        demanda = gera_demanda(3, dados)
+        ct = simula(dados, melhor_E, melhor_T, demanda)
+        custos.append(ct)
+    print(np.mean(custos))
+   
     #>>>>> Cenário - 4: A média da demanda pode alterar para cima ou para baixo
-    #demanda = gera_damanda(4, dados)
     #custos = []
     #for i in range(100):
-    #    ct = simula(dados, E, T, demanda)
+    #    demanda = gera_demanda(4, dados)
+    #    ct = simula(dados, melhor_E, melhor_T, demanda)
     #    custos.append(ct)
     #print(np.mean(custos))
+ 
+   
+   
 
        
 
